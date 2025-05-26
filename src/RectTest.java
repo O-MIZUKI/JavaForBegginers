df57178fc63505b6449f13140d35c55d9756b903
MSI\hisui <zeal2460060@fsgci.jp>
2025-05-26 10:14:03 +0900

    no message

---
+++ src/RectTest.java
@@ -0,0 +1,19 @@
+
+public class RectTest extends MyFrame{
+
+	public void run() {
+		setColor(255,0,255);
+		fillRect(30,40,30,30);
+		setColor(255,255,0);
+		fillRect(60,40,50,30);
+		setColor(0,255,255);
+		fillRect(90,40,30,30);
+		
+		setColor(0,0,0);
+		fillRect(30,70,30,30);
+		setColor(105,105,105);
+		fillRect(60,70,30,30);
+		
+	}
+
+}
---
+++ src/Start.java
@@ -0,0 +1,9 @@
+
+
+public class Start  {
+		public static void main(String[] args) {
+			System.out.println("division");
+			MyFrame frame1 = new RectTest();
+	}
+
+}
