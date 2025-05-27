69c2afea51d769044061af7f3b8c3f51fe256080
MSI\hisui <zeal2460060@fsgci.jp>
2025-05-27 10:36:21 +0900

    DisplayRectsSlow

--- src/RectIf.java
+++ src/RectIf.java
@@ -1,18 +1,18 @@
 
 public class RectIf extends MyFrame{
-	public void run(){
+	public void run() {
 		int x = 30;
-		int y = 128;
+		int y = 0;
 		
 		int i;
 		for(i = 0;i<20; i++) {
 			if(i>10) {
-				setColor(0,y,0);
+				setColor(y,y,y);
 				fillRect(x,80,10,100);	
 				
 				y -= 20;
 			}else {
-			setColor(0,y,0);
+			setColor(y,y,y);
 			fillRect(x,80,10,100);	
 			y += 20;
 			}
