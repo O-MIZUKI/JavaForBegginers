d84dfb3b4c3cc9571b46ac665ae0ed1a66aa7cea
MSI\hisui <zeal2460060@fsgci.jp>
2025-05-27 12:22:42 +0900

    5-1,5-2,5-3

--- src/Animation.java
+++ src/Animation.java
@@ -4,7 +4,7 @@ public class Animation extends MyFrame{
 		int x = 30;
 		int y =100;
 		setColor(0,128,0);
-		while(true) {
+		/*while(true) {
 			while(y>=50) {
 				clear();
 				fillOval(x,y,40,40);
@@ -19,6 +19,51 @@ public class Animation extends MyFrame{
 				x += 5;
 				sleep(0.1);
 			} 
+		}*/
+		
+		/*while(x<=170) {
+			clear();
+			fillOval(x,y,40,40);
+			y += 5;
+			x += 5;
+			sleep(0.1);
+		} 
+		while(x>=40) {
+			clear();
+			fillOval(x,y,40,40);
+	
+			x -= 5;
+			sleep(0.1);
+		} 
+		
+		while(x<=170) {
+			clear();
+			fillOval(x,y,40,40);
+			y -= 5;
+			x += 5;
+			sleep(0.1);
+		} */
+		
+		
+		
+		for(int i = 0; i<3;i++) {
+			while(x<=170) {
+				clear();
+				fillOval(x,y,40,40);
+		
+				x += 5;
+				sleep(0.1);
+			} 
+			while(x>=30) {
+				clear();
+				fillOval(x,y,40,40);
+		
+				x -= 5;
+				sleep(0.1);
+			
+			} 
 		}
+		
+		
 	}
 }
