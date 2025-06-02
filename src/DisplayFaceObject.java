892c72440024bac79730eaef51c1ecc082eedff2
MSI\hisui <zeal2460060@fsgci.jp>
2025-06-02 11:40:56 +0900

    6-2

--- src/DisplayFaceObject.java
+++ src/DisplayFaceObject.java
@@ -1,14 +1,14 @@
 
 public class DisplayFaceObject extends MyFrame{
 	public void run(){
-		Face facel=new Face();
-		facel.x=50;
-		facel.y=50;
-		facel.draw(this);
+		Face facel=new Face(50,50);
+		Face facel2=new Face(200,100);
+		Face facel3=new Face(250,50);
 		
-		Face facel2=new Face();
-		facel2.x=200;
-		facel2.y=150;
+		facel.draw(this);
 		facel2.draw(this);
+		facel3.draw(this);
+		
+		
 	}
 }
