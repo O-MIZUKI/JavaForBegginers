e5238fa88bdd2418e14d9dae9f1daa620a4f2258
MSI\hisui <zeal2460060@fsgci.jp>
2025-06-02 12:19:01 +0900

    Tree

--- src/Tree.java
+++ src/Tree.java
@@ -1,6 +1,6 @@
-public class Face {
+public class Tree {
 	int x,y,vx,vy;
-	public Face(int x,int y,int vx, int vy) {
+	public Tree(int x,int y,int vx, int vy) {
 		this.x=x;
 		this.y=y;
 		this.vx=vx;
