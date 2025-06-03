2e0909ba73ebd90df56c879a8f1857cd3d049550
MSI\hisui <zeal2460060@fsgci.jp>
2025-06-03 12:11:13 +0900

    7-1

--- src/DisplayFaceVector.java
+++ src/DisplayFaceVector.java
@@ -2,14 +2,17 @@ import java.util.Vector;
 
 public class DisplayFaceVector extends MyFrame{
 	public void run(){
-		Vector<Face>faces=new Vector<Face>();
-		faces.add(new Face(50,50,10,5));
-		faces.add(new Face(200,100,-10,-5));
+		Vector<Car>cars=new Vector<Car>();
+		cars.add(new Car(50,50,10,5));
+		cars.add(new Car(50,150,11,5));
+		cars.add(new Car(50,250,12,5));
+		cars.add(new Car(50,350,13,5));
+		cars.add(new Car(50,450,14,5));
 		for (int i=0; i<30;i++) {
 			clear();
-			for(int j=0;j<faces.size();j++) {
-			faces.get(j).draw(this);
-			faces.get(j).move();
+			for(int j=0;j<cars.size();j++) {
+			cars.get(j).draw(this);
+			cars.get(j).move();
 			}
 			sleep(0.1);
 		}
